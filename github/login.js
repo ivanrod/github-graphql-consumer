e7e5e7611639e4bd754168aaec7b4a6ec15e2374
Iván Rodriguez <ivan.rodriguez@bq.com>
2016-11-05 21:52:39 +0100

    Add github login module

---
+++ github/login.js
@@ -0,0 +1,40 @@
+const base64 = require('base-64');
+const fetch = require('node-fetch');
+const config = require('config');
+
+const credentials = {
+  GITHUB_CLIENT_ID: config.get('credentials.github.clientId'),
+  GITHUB_CLIENT_SECRET: config.get('credentials.github.clientSecret'),
+};
+
+const AUTH_URL_PATH = 'https://api.github.com/authorizations';
+
+function login(name, pwd) {
+  const bytes = name.trim() + ':' + pwd.trim();
+  const encoded = base64.encode(bytes);
+
+  return fetch(AUTH_URL_PATH, {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Basic ' + encoded,
+      'Content-Type': 'application/json; charset=utf-8',
+      'Accept': 'application/vnd.github.inertia-preview+json'
+    },
+    body: JSON.stringify({
+      'client_id': credentials.GITHUB_CLIENT_ID,
+      'client_secret': credentials.GITHUB_CLIENT_SECRET,
+      'scopes': ['user', 'repo'],
+      'note': 'not abuse'
+    })
+  }).then(response => response.json()
+      .then(json => {
+        if (response.status < 400) {
+          return json.token;
+        } else {
+          throw new Error(json.message);
+        }
+      })
+    );
+}
+
+module.exports = login;
