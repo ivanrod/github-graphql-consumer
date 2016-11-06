c1de8f7122b93cb95b1a3122f218071d15d6e0f0
Iván Rodriguez <ivan.rodriguez@bq.com>
2016-11-06 03:13:35 +0100

    Add co to simplify generator control flow

--- github/login.js
+++ github/login.js
@@ -1,6 +1,7 @@
 const base64 = require('base-64');
 const fetch = require('node-fetch');
 const config = require('config');
+const co = require('co');
 
 const credentials = {
   GITHUB_CLIENT_ID: config.get('credentials.github.clientId'),
@@ -9,11 +10,30 @@ const credentials = {
 
 const AUTH_URL_PATH = 'https://api.github.com/authorizations';
 
-function login(name, pwd) {
-  const bytes = name.trim() + ':' + pwd.trim();
+function* resolveRequest(request) {
+  const response = yield request;
+  const json = yield response.json();
+
+  if (response.status < 400) {
+    return json.token;
+  } else {
+    let error = new Error(json.message);
+    error.status = response.status;
+    throw error;
+  }
+}
+
+/**
+ * Login with github
+ * @param  {string} user
+ * @param  {string} pwd
+ * @return {Promise}  Token
+ */
+function login(user, pwd) {
+  const bytes = user.trim() + ':' + pwd.trim();
   const encoded = base64.encode(bytes);
 
-  return fetch(AUTH_URL_PATH, {
+  const request = fetch(AUTH_URL_PATH, {
     method: 'POST',
     headers: {
       'Authorization': 'Basic ' + encoded,
@@ -26,24 +46,16 @@ function login(name, pwd) {
       'scopes': ['user', 'repo'],
       'note': 'not abuse'
     })
-  }).then(response => response.json()
-      .then(json => {
-        if (response.status < 400) {
-          return json.token;
-        } else {
-          let error = new Error(json.message);
-          error.status = response.status;
-          throw error;
-        }
-      })
-    );
+  })
+
+  return co(resolveRequest(request));
 }
 
+// Default export to work as a koa middleware
 module.exports = function *() {
   try {
     yield login.apply(this, arguments).then(token => this.body = {token});
   } catch (err) {
-
     if (!err.status) {
       this.status = 500;
     } else {
