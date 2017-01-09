912e10b58ede12bdf4d59a0f0f90586c008f5d28
Iván Rodriguez <ivan.rodriguez@bq.com>
2017-01-09 12:22:33 +0100

    Improve lastMonthsCommits query

--- src/github/queries.js
+++ src/github/queries.js
@@ -82,6 +82,7 @@ function repositories() {
                             id
                             name
                           }
+                          date
                         }
                       }
                     }
